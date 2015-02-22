916b3728cc99f12a7453ea37691f7023b47b996f
Mr. White <s.ciarro@gmail.com>
2015-02-21 20:14:56 -0500

    Menu

--- wp-content/plugins/js_composer/include/templates/shortcodes/vc_accordion_tab.php
+++ wp-content/plugins/js_composer/include/templates/shortcodes/vc_accordion_tab.php
@@ -7,7 +7,6 @@ extract(shortcode_atts(array(
 
 $css_class = apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, $this->settings['base'], $atts );
 $output .= "\n\t\t\t" . '<div class="'.$css_class.'">';
-    $output .= "\n\t\t\t\t" . '<h2>'.$title.'test</h2>';
     $output .= "\n\t\t\t\t" . '<div class="vc_clearfix">';
         $output .= ($content=='' || $content==' ') ? __("Empty section. Edit page to add content here.", "js_composer") : "\n\t\t\t\t" . wpb_js_remove_wpautop($content);
         $output .= "\n\t\t\t\t" . '</div>';
