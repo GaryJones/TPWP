535458c393f5a1e76e4ef0d0e074e28e8be665bc
Mr. White <s.ciarro@gmail.com>
2015-03-01 21:09:51 -0500

    Menu

--- wp-content/themes/willow/header.php
+++ wp-content/themes/willow/header.php
@@ -238,10 +238,10 @@ if ( $enable_parallax ) wp_enqueue_script( 'jquery-parallax' ); ?>
 							<img src="http://www.thepricewepay.ca/wp-content/uploads/2015/03/LG_FESTIVAL_256x91_10.png"/>
 						</li>
 						<li>
-							<img src="http://www.thepricewepay.ca/wp-content/uploads/2015/03/LG_FESTIVAL_256x91_11.png"/>
+							<img src="http://www.thepricewepay.ca/wp-content/uploads/2015/03/LG_FESTIVAL_250x91_11.png"/>
 						</li>
 						<li>
-							<img src="http://www.thepricewepay.ca/wp-content/uploads/2015/03/LG_FESTIVAL_256x91_12.png"/>
+							<img src="http://www.thepricewepay.ca/wp-content/uploads/2015/03/LG_FESTIVAL_250x91_12.png"/>
 						</li>
 					</ul>
 				</div>
